bc5ec42a3a8fd5ee37131c466fa80d55cfd3dad6
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2022-06-21 12:14:51 +0200

    Add generator

--- fibonacci/__init__.py
+++ fibonacci/__init__.py
@@ -2,6 +2,7 @@ from functools import cache
 
 __all__ = [
     'fibonacci',
+    'fibonacci_generator',
 ]
 
 @cache
@@ -11,3 +12,20 @@ def fibonacci(n):
     if n == 1:
         return 1
     return fibonacci(n - 1) + fibonacci(n - 2)
+
+
+def fibonacci_generator(n):
+    yield 0
+
+    if n == 0:
+        return
+
+    yield 1
+
+    if n == 1:
+        return
+
+    n0, n1 = 0, 1
+    for _ in range(2, n):
+        n0, n1 = n1, n0 + n1
+        yield n1
