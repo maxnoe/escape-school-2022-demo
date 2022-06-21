7c27dd81504615a7c03dd0bd37dc4cec059cfb51
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2022-06-21 09:41:46 +0200

    Add CLI to fibonacci

--- fibonacci.py
+++ fibonacci.py
@@ -1,6 +1,20 @@
+from argparse import ArgumentParser
+
+parser = ArgumentParser()
+parser.add_argument('n', type=int)
+
 def fibonacci(n):
     if n == 0:
         return 0
     if n == 1:
         return 1
     return fibonacci(n - 1) + fibonacci(n - 2)
+
+
+def main():
+    args = parser.parse_args()
+    print(fibonacci(args.n))
+
+
+if __name__ == '__main__':
+    main()
