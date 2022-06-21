29fd8f272d514447e7fa5bc746442c08f74fe342
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2022-06-21 09:47:02 +0200

    Change project structure to a python package

--- fibonacci/__main__.py
+++ fibonacci/__main__.py
@@ -1,15 +1,8 @@
 from argparse import ArgumentParser
+from . import fibonacci
 
 parser = ArgumentParser()
 parser.add_argument('n', type=int)
-
-def fibonacci(n):
-    if n == 0:
-        return 0
-    if n == 1:
-        return 1
-    return fibonacci(n - 1) + fibonacci(n - 2)
-
 
 def main():
     args = parser.parse_args()
