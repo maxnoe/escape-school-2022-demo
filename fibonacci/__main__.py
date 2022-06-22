ab4f2c33cf210417f27290cbb3868bc7e0ee13d6
Stefan Fröse <stefan.froese@udo.edu>
2022-06-22 15:04:12 +0200

    add option -t for plotting fib numbers as tiles

--- fibonacci/__main__.py
+++ fibonacci/__main__.py
@@ -1,15 +1,18 @@
 from argparse import ArgumentParser
-from . import fibonacci
+from . import fibonacci, fibonacci_tiling
 
 parser = ArgumentParser()
 parser.add_argument('n', type=int)
 parser.add_argument('-l', '--list', action='store_true')
+parser.add_argument('-t', '--tiles', action='store_true')
 
 def main():
     args = parser.parse_args()
     if args.list:
         for i in range(args.n + 1):
             print(fibonacci(i))
+    if args.tiles:
+        fibonacci_tiling(args.n)
     else:
         print(fibonacci(args.n))
 
